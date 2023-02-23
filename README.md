f495bbe59d1c6940498be5bf528c1e866741d12b
abdulkareemAbunabhan <zman17881@gmail.com>
2023-02-23 04:02:17 +0000

    created server

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Movies-Library
+# Movies-Library - V 1.0.0
+
+**Author Name**: Abdulkareem Abunabhan
+
+## WRRC
+
+![WRRC-img](./SPOILER_Untitled_1.jpg)
+
+## Overview
+
+this application is tracking the new movies from all categries.
+
+## Getting Started
+
+web browser will be good
+
+## Project Features
+
+Just getting starting.
---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict'
+
+//Importing express
+const express = require('express');
+//Import cors
+const cors = require('cors');
+
+const server = express();
+
+//PORT 
+const PORT = 3000;
+
+
+//opening the server for any user
+server.use(cors());
+
+//Routes
+//home
+server.get('/',(req,res)=>{
+    res.status(200).send('constructer')
+})
+//favotite
+server.get('/favorite',(req,res)=>{
+    res.send('favorite');
+})
+//Something wrong
+//dafault
+server.get('*',(req,res)=>{
+    res.status(404).send('No such page')
+})
+//PORT listen
+server.listen('PORT',()=>{
+    console.log(`listening on ${PORT}`)
+})
